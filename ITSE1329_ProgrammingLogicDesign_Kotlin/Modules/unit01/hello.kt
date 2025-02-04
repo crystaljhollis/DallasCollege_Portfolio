4ca0d95eac1607ce983c097ec29704324da8daa2
Crystal Hollis <crystaljhollis@gmail.com>
2025-02-03 23:09:25 -0600

    Updating Repository 2-3-25
    Updating Repository 2-3-25

--- ITSE1329_ProgrammingLogicDesign_Kotlin/Modules/unit01/hello.kt
+++ ITSE1329_ProgrammingLogicDesign_Kotlin/Modules/unit01/hello.kt
@@ -1,4 +1,5 @@
 fun main() {
+  println("Debug Program started")
   println("Hello World!")
   println("I am learning Kotlin.")
   println("It will be the foundation for my understanding of programming languages!")
